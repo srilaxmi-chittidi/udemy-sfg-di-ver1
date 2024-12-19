a8aa84724a969d5a44b65f86162cd9660f7fa2be
srilaxmi-chittidi <laxmi.chittidi@gmail.com>
2024-12-19 16:32:46 -0500

    Added XML Configuration closes #4

--- sfg-di/src/main/java/com/sfg/sfg_di/config/GreetingServiceConfig.java
+++ sfg-di/src/main/java/com/sfg/sfg_di/config/GreetingServiceConfig.java
@@ -2,6 +2,7 @@ package com.sfg.sfg_di.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
@@ -16,13 +17,14 @@ import com.sfg.sfg_di.service.SetterInjectedServiceImpl;
 import com.sfg.sfg_di.service.i18NEnglishGreetingService;
 import com.sfg.sfg_di.service.i18NSpanishGreetingService;
 
+@ImportResource("classpath:sfg-config.xml")
 @Configuration
 public class GreetingServiceConfig {
 
-	@Bean
-	SetterInjectedServiceImpl setterInjectedServiceImpl() {
-		return new SetterInjectedServiceImpl();
-	}
+//	@Bean
+//	SetterInjectedServiceImpl setterInjectedServiceImpl() {
+//		return new SetterInjectedServiceImpl();
+//	}
 	
 	@Bean
 	PropertyInjectedGreetingServiceImpl propertyInjectedGreetingServiceImpl() {
